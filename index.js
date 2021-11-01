92be23805267b2c998f3cd89a1f738fda4754063
Konstantin Chudinov <chudinov.constantine@gmail.com>
2021-11-01 13:10:12 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ *  Each element in the array represents your maximum jump length at that position.
+ *  Your goal is to reach the last index in the minimum number of jumps.
+ *  You can assume that you can always reach the last index.
+ * @param {number[]} nums array of numbers from [0..3000]
+ * @return {number} min number of jumps to reach last element.
+ */
+
+var jump = function(nums) {
+    // Your code here
+};
+
+module.exports = jump;
